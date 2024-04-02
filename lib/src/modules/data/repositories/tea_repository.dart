c1c5008072f2cf00854b6ad1c0a4f001576ff697
ruan <ruan@ae3tecnologia.com.br>
2024-04-02 14:11:48 -0400

    feat creat tea repository data

---
+++ lib/src/modules/data/repositories/tea_repository.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import '../../domain/entities/tea_entity.dart';
+import '../../domain/errors/tea_errors.dart';
+import '../../domain/respositories/repositories.dart';
+
+class TeaRepository implements ITeaRepository {
+  @override
+  Future<Either<TeaErrors, List<TeaEntity>>> getTea() {
+    // TODO: implement getTea
+    throw UnimplementedError();
+  }
+}
