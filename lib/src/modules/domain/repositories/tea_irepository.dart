5c1e3f20524952ff8b47c4bb4b25c2d8b1807033
ruan <rhuanjanau531@gmail.com>
2024-04-10 15:47:41 -0400

    feat craete irepository data

---
+++ lib/src/modules/domain/repositories/tea_irepository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+
+import '../entities/entities.dart';
+import '../errors/errors.dart';
+
+abstract class ITeaRepository {
+  Future<Either<TeaErrors, List<TeaEntity>>> getTea();
+}
