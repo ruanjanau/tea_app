c52b06d711a4b4842714f079206bb2f98d360312
ruan <rhuanjanau531@gmail.com>
2024-04-10 15:46:35 -0400

    delete file

--- lib/src/modules/domain/respositories/tea_irepository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-
-import '../entities/entities.dart';
-import '../errors/errors.dart';
-
-abstract class ITeaRepository {
-  Future<Either<TeaErrors, List<TeaEntity>>> getTea();
-}
