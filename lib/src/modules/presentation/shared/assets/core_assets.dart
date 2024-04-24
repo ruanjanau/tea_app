854a9b716706ce442d07f766ca48bdb1b38e3e2c
ruan <rhuanjanau531@gmail.com>
2024-04-24 12:05:20 -0400

    delete files

--- lib/src/modules/presentation/shared/assets/core_assets.dart
+++
@@ -1,9 +0,0 @@
-class CoreAssets {
-  static String get titleApp => 'assets/svg/titileapp.svg';
-
-  static String get splash => 'assets/svg/plant.svg';
-
-  static String get onbo1 => 'assets/svg/onbo1.svg';
-
-  static String get onbo2 => 'assets/svg/onbo2.svg';
-}
--- lib/src/modules/presentation/shared/widgets/widgets.dart
+++
@@ -1,4 +0,0 @@
-export 'custom_button.dart';
-export 'custom_card_product.dart';
-export 'custom_text_field.dart';
-export 'custom_title_app.dart';
